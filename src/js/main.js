cc04a9b871ef9395c91bd6d282236723cdae0e85
connermurphy <chuckieminster@aol.com>
2020-06-15 03:36:34 +0100

    Added project overview modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,57 @@
 $(document).ready(() => {
 
+    let modalData = [
+        {
+            id: 0,
+            title: "A Weekend In Glasgow",
+            desc: "Built as part of a team working exercise for college, this prototype website showcased 5 things to do in Glasgow.",
+            photos: [
+                "./assets/img/work/awig/awig.png",
+                "./assets/img/work/awig/awig_2.png",
+                "./assets/img/work/awig/awig_3.png"
+            ],
+            langs: [
+                "HTML",
+                "CSS",
+                "JS"
+            ]
+        },
+        {
+            id: 1,
+            title: "Jinxx - Portfolio",
+            desc: "Commissioned to build a fully functional portfolio website for a social media manager.",
+            photos: [
+                "./assets/img/work/jinxx/jinxx.png",
+                "./assets/img/work/jinxx/jinxx_2.png",
+                "./assets/img/work/jinxx/jinxx_3.png",
+            ],
+            langs: [
+                "HTML",
+                "CSS / SASS",
+                "JS / jQuery",
+                "PHP"
+            ]
+        },
+        {
+            id: 2,
+            title: "Spectral Radio",
+            desc: "The landing page plays live music with action buttons and displays some information about the radio station.",
+            photos: [
+                "./assets/img/work/spectral/spectral.png",
+                "./assets/img/work/spectral/spectral_2.png"
+            ],
+            langs: [
+                "HTML",
+                "CSS / SASS / TailwindCSS",
+                "JS / jQuery"
+            ]
+        },
+    ];
+
+    let slick = 0;
+
+    let projectModal = $("#project-modal");
+
     let navbarTogglerOne = $("#toggler--1");
     let navbarTogglerTwo = $("#toggler--2");
     let navbarTogglerThree = $("#toggler--3");
@@ -13,18 +65,19 @@ $(document).ready(() => {
         }
 
         if ($(document).scrollTop() > $("#work--section").offset().top - 200) {
-            $(".work--item").eq(0).attr("id", "animate--work-1");
-            $(".work--item").eq(1).attr("id", "animate--work-2")
+            $(".work--item").eq(0).addClass("fadeIn").css("animation-delay", ".25s");
+            $(".work--item").eq(1).addClass("fadeIn").css("animation-delay", ".5s");
+            $(".work--item").eq(2).addClass("fadeIn").css("animation-delay", ".75s");
         }
-        
-        if ($(document).scrollTop() > $("#service--section").offset().top - 200) {            
+
+        if ($(document).scrollTop() > $("#service--section").offset().top - 200) {
             $(".service--item").eq(0).attr("id", "animate--service-1");
             $(".service--item").eq(1).attr("id", "animate--service-2");
             $(".service--item").eq(2).attr("id", "animate--service-3");
         }
 
-        if ($(document).scrollTop() > $("#contact--section").offset().top -  200) {
-            setTimeout(function() {
+        if ($(document).scrollTop() > $("#contact--section").offset().top - 200) {
+            setTimeout(function () {
                 displayPlaceholder($("#form--name"), "John Doe");
                 displayPlaceholder($("#form--email"), "johndoe@mail.com");
                 displayPlaceholder($("#form--message"), "Hi Conner, I'm looking for a new website..");
@@ -32,7 +85,62 @@ $(document).ready(() => {
         }
     });
 
-    //displayPlaceholder($("#form--email"), "johndoe@live.com");
+    function initCarousel() {
+        $(".project--carousel").slick({
+            dots: true,
+            lazyLoad: 'ondemand',
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows : false,
+            autoplay : true,
+            infinite : true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+            ]
+        });
+    }
+
+    $(".work--item-img").on("click", function () {
+
+        if (slick > 0) {
+            $(".project--carousel").slick("unslick");
+        }
+
+        let clickedWorkItem = $(".work--item-img").index(this);
+        let projectData = modalData[clickedWorkItem];
+
+        $(projectModal).find(".modal-title").text(projectData.title);
+        $(projectModal).find(".modal-sub").text(projectData.desc);
+
+        $(projectModal).find(".project--carousel").html("");
+        $(projectModal).find(".project--langs").html("");
+
+        for (let i = 0; i < projectData.photos.length; i++) {
+            $(projectModal).find(".project--carousel").append(`<img src="${projectData.photos[i]}" class="project--carousel-img" />`)
+        }
+
+        for (let i = 0; i < projectData.langs.length; i++) {
+            $(projectModal).find(".project--langs").append(`<li class="text-black-50">${projectData.langs[i]}</li>`);
+        }
+
+        initCarousel();
+        $(projectModal).modal("show");
+        slick++;
+    });
+
 
     $("#contact--form").on("submit", (e) => {
 
@@ -45,17 +153,17 @@ $(document).ready(() => {
 
         $.ajax({
 
-            "url" : "/assets/php/contact.php",
-            "method" : "POST",
-            "data" : {
-                name : nameVal,
-                email : emailVal,
-                message : messageVal,
-                website : websiteVal,
-                grecaptcha : grecaptcha.getResponse()
+            "url": "/assets/php/contact.php",
+            "method": "POST",
+            "data": {
+                name: nameVal,
+                email: emailVal,
+                message: messageVal,
+                website: websiteVal,
+                grecaptcha: grecaptcha.getResponse()
             },
-            success : function(res) {
-                
+            success: function (res) {
+
                 if (res === "1") {
                     //success
                     $("#form--msg-status").text("Email sent successfully");
@@ -70,25 +178,25 @@ $(document).ready(() => {
 
                 $("#contact--form")[0].reset();
 
-                setTimeout(function() {
+                setTimeout(function () {
                     $("#form--msg-status").text("");
                 }, 4000);
 
             },
-            error : function(err) {
+            error: function (err) {
                 console.log(err)
             }
         });
     });
 
-    $("#header--content-icon").on("click", function() {
+    $("#header--content-icon").on("click", function () {
         smoothScroll($("#work--section"));
     });
 
-    $(".nav-link").on("click", function() {
+    $(".nav-link").on("click", function () {
 
         let navText = $(this).text().toLowerCase().trim();
-        switch(navText) {
+        switch (navText) {
 
             case "work":
                 smoothScroll($("#work--section"));
@@ -110,7 +218,7 @@ $(document).ready(() => {
             $(navbarTogglerThree).addClass("toggler--rotate-3");
 
         } else {
-            
+
             $(navbarTogglerOne).removeClass("toggler--rotate-1");
             $(navbarTogglerThree).removeClass("toggler--rotate-3");
             $(navbarTogglerTwo).css("visibility", "visible");
@@ -123,11 +231,11 @@ $(document).ready(() => {
         let elementTarget = $(element).attr("data-value");
         let currentValue = $(element).text();
 
-        let countStatInterval = setInterval(function() {
+        let countStatInterval = setInterval(function () {
 
             if (currentValue < elementTarget) {
                 ++currentValue;
-                $(element).text(currentValue);               
+                $(element).text(currentValue);
             } else {
                 clearInterval(countStatInterval);
             }
@@ -135,32 +243,32 @@ $(document).ready(() => {
         }, (elementTarget * 10));
 
     }
-    
+
     function smoothScroll(section) {
 
         let sectionOffset = $(section).offset().top;
 
         $("html, body").animate({
-            "scrollTop" : sectionOffset
+            "scrollTop": sectionOffset
         }, 1000);
 
     }
-    
+
     function displayPlaceholder(element, placeholderText) {
 
 
-        let placeholderInterval = setInterval(function() {
+        let placeholderInterval = setInterval(function () {
             if ($(element).attr("placeholder") !== placeholderText) {
                 $(element).attr("placeholder", $(element).attr("placeholder") + placeholderText.charAt(parseInt($(element).attr("placeholder-val"))))
                 $(element).attr("placeholder-val", parseInt($(element).attr("placeholder-val")) + 1);
-                          
+
             } else {
                 clearInterval(placeholderInterval);
             }
-            
+
 
         }, 175);
 
-    } 
+    }
 
 });
